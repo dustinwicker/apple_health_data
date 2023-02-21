87f4ade985fa6cd8991970c82ad0c5bb586db00f
Dustin Wicker <31931512+dustinwicker@users.noreply.github.com>
2023-02-20 22:09:58 -0500

    visual hr

---
+++ apple_health_data.py
@@ -0,0 +1,83 @@
+import os
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Increase maximum width in characters of columns - will put all columns in same line in console readout
+pd.set_option('expand_frame_repr', False)
+# Increase number of rows printed out in console
+pd.options.display.max_rows = 200
+pd.options.display.min_rows = None
+# Be able to read entire value in each column (no longer truncating values)
+pd.set_option('display.max_colwidth', None)
+np.set_printoptions(threshold=np.inf, suppress=True)
+
+# Change and update directory
+os.chdir("C:\\Users\wickerd\Desktop\\apple_health_export")
+# List files in current directory by last modified time
+files = list(filter(os.path.isfile, os.listdir(os.getcwd())))
+files.sort(key=lambda x: os.path.getmtime(x), reverse=True)
+print(files)
+
+import lxml
+export_df = pd.read_xml('export.xml')
+export_df_copy = export_df.copy()
+
+export_df.columns
+export_df.dtypes
+export_df[:10]
+df = export_df[['value','startDate','unit']]
+df.columns
+df.unit.value_counts()
+df.loc[df.unit=='ft']
+df.loc[df.unit=='lb']
+df.loc[df.unit=='mi']
+
+df=df[4:]
+df['startDate'] = pd.to_datetime(df['startDate'].str[:-6])
+df['value'].astype(float)
+df.dtypes
+
+df.loc[df.value=='HKCategoryValueSleepAnalysisAsleep']
+df[2214020:2218395]
+export_df[2214020:2218395]
+
+df.shape
+df = df.loc[df.unit.notnull()]
+df['value'] = df['value'].astype(float)
+
+for i in df['unit'].value_counts().index:
+    i
+    df.loc[df.unit==i,'value'].describe()
+    print('\n')
+
+set(export_df['HKCharacteristicTypeIdentifierDateOfBirth'])
+set(export_df['HKCharacteristicTypeIdentifierBloodType'])
+set(export_df['workoutActivityType'])
+df = df.set_index('startDate')
+df.loc[df.unit=='count/min','value'].describe()
+df.loc[df.unit=='count/min'][:500][['value', 'startDate']].plot()
+plt.plot(df.loc[df.unit=='count/min']['value'])
+plt.xticks(df.loc[df.unit=='count/min']['startDate'])
+plt.show()
+df.index.value_counts().sort_index()
+
+
+for c in export_df.columns:
+    print(export_df[c].unique())
+    print('-')
+    print(' ')
+
+# Change and update directory
+os.chdir("C:\\Users\wickerd\Desktop\\apple_health_export\electrocardiograms")
+
+# List files in current directory by last modified time
+files = list(filter(os.path.isfile, os.listdir(os.getcwd())))
+files.sort(key=lambda x: os.path.getmtime(x), reverse=True)
+print(files)
+
+# Read one of the csv files
+ecg = pd.read_csv('ecg_2019-09-21.csv')
+ecg.shape
+ecg[:10]
+ecg[9:].shapedf[]
